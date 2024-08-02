7f4be98d7e5c4f6218a1427fae4cd4cbd8173564
unknown <rijal.1344@gmail.com>
2024-08-02 12:25:48 +0700

    core: add seed

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('user_roles')->insert([
+            'user_id' => 1,
+            'role_id' => 1
+        ], [
+            'user_id' => 2,
+            'role_id' => 2
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'username' => 'admin',
+            'password' => '$2a$10$u2A1gEVJDaIonQLLyZNnV.UH//Hpjly12XRnZGGWHDqhJ7xAXMsS6'
+        ], [
+            'name' => 'author',
+            'username' => 'author',
+            'password' => '$2a$10$SA3uo4bwjuZYbwQiN6ZJKuZTEhQFIdj2YDDVrDR9nWlIBshpubaeS'
+        ]);
+    }
+}
